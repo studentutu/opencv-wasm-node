f7a89cd7cbacc3306b8ac1bf90db3598e8abe865
Cezary Nowak <cezary.nowak@gmail.com>
2024-06-19 00:19:33 +0200

    4.5.5 revisited

--- utils/build.sh
+++ utils/build.sh
@@ -1,9 +1,10 @@
-git clone --branch 4.3.0 --depth 1 https://github.com/opencv/opencv.git
+git clone --branch 4.5.5 --depth 1 https://github.com/opencv/opencv.git
 
 # Build
 (
     cd opencv &&
-    git checkout 4.3.0 &&
+    git fetch --tags &&
+    git checkout 4.5.5 &&
 
     # Add non async flag before compiling in the python build_js.py script
     docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:sdk-tag-1.39.4-64bit" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test --build_flags "-s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s SINGLE_FILE=0 "
